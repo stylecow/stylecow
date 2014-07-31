4e2c94b7d783f568b85d1a673ab97b358e56345a
oscarotero <oom@oscarotero.com>
2014-07-31 21:32:13 +0200

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ stylecow
 ========
 
 Modern CSS for all browsers
+
+* Created by Oscar Otero <http://oscarotero.com> / <oom@oscarotero.com>
+* License: MIT
+
+Node library to fix your css code and make it compatible with all browsers:
---
+++ tests/utils.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var utils  = require('../lib/utils');
+
+var r;
+
+r = utils.explode(' ', 'one (two three) "four"');
+assert.deepEqual(r, ['one', '(two three)', '"four"']);
+
+r = utils.explode(' ', 'one (two three) "four"', 2);
+assert.deepEqual(r, ['one', '(two three) "four"']);
+
+r = utils.explode(' ', 'one (two \'three \"four"');
+assert.deepEqual(r, ['one', '(two \'three \"four"']);
+
+r = utils.explodeTrim(';', 'one ; two; three;');
+assert.deepEqual(r, ['one', 'two', 'three']);
+
+r = utils.explode('{', 'div.ola {font-size: 3em; a {color: blue;}}', 2);
+assert.deepEqual(r, ['div.ola ', 'font-size: 3em; a {color: blue;}}']);
+
+r = utils.explodeTrim(',', 'one');
+assert.deepEqual(r, ['one']);

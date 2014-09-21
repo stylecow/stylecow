7018dcdf85a57bcc2ae6415c459e9184adb60f28
oscarotero <oom@oscarotero.com>
2014-09-21 15:33:58 +0200

    Deep changes in the library structure

--- tests/css.js
+++ tests/css.js
@@ -1,47 +1,46 @@
 var assert = require('assert');
-
-var css = require('../lib/css');
+var stylecow = require('../lib');
 var r;
 
-r = css.Function.create('rgba(23,   24, 56,   .4)');
+r = stylecow.Function.create('rgba(23,   24, 56,   .4)');
 assert.strictEqual(r.toString(), 'rgba(23, 24, 56, .4)');
 
-r = css.Declaration.create('font-family: Arial');
+r = stylecow.Declaration.create('font-family: Arial');
 assert.strictEqual(r.toString(), 'font-family: Arial;');
 
-r = css.Declaration.create('background-color: rgba(23, 24, 56, .4)');
+r = stylecow.Declaration.create('background-color: rgba(23, 24, 56, .4)');
 assert.strictEqual(r.toString(), 'background-color: rgba(23, 24, 56, .4);');
 
-r = css.Declaration.create('width: calc(2 + 3)');
+r = stylecow.Declaration.create('width: calc(2 + 3)');
 assert.strictEqual(r.search('Function', 'calc')[0].name, 'calc');
 
-r = css.Selector.create('div.ola');
+r = stylecow.Selector.create('div.ola');
 assert.strictEqual(r.toString(), 'div.ola');
 
-r = css.Rule.create('div.ola {font-size: 3em;}');
+r = stylecow.Rule.create('div.ola {font-size: 3em;}');
 assert.strictEqual(r.toString(), 'div.ola {\n\tfont-size: 3em;\n}');
 
-r = css.Rule.create('foo {font-size: calc(3em + (5px * 4px));}');
+r = stylecow.Rule.create('foo {font-size: calc(3em + (5px * 4px));}');
 assert.strictEqual(r.toString(), 'foo {\n\tfont-size: calc(3em + (5px * 4px));\n}');
 
-r = css.Function.create('linear-gradient(rgba(23,   24, 56,   .4), #333)');
+r = stylecow.Function.create('linear-gradient(rgba(23,   24, 56,   .4), #333)');
 assert.strictEqual(r.toString(), 'linear-gradient(rgba(23, 24, 56, .4), #333)');
 
-r = css.Selector.create('div.ola');
+r = stylecow.Selector.create('div.ola');
 assert.strictEqual(r.toString(), 'div.ola');
 assert.strictEqual(r.length, 2);
 
 
-r = css.Keyword.create('.ola');
+r = stylecow.Keyword.create('.ola');
 assert.strictEqual(r.toString(), '.ola');
 
-r = css.Function.create('::matches(div,p  )');
+r = stylecow.Function.create('::matches(div,p  )');
 assert.strictEqual(r.toString(), '::matches(div, p)');
 
-r = css.Selector.create('div.ola::after');
+r = stylecow.Selector.create('div.ola::after');
 assert.strictEqual(true, r.has('Keyword', '::after'));
 
-r = css.Rule.create('div.ola::after {color: blue;}');
+r = stylecow.Rule.create('div.ola::after {color: blue;}');
 assert.strictEqual(true, r.has('Keyword', '::after'));
 assert.strictEqual(r, r.search('Keyword', '::after')[0].ancestor('Rule'));
 
@@ -49,18 +48,18 @@ var clone = r.search('Selector')[0].clone();
 r.add(clone);
 assert.strictEqual(r, clone.ancestor('Rule'));
 
-r = css.Rule.create('div.ola::after {color: blue; font-size: 2em;}');
+r = stylecow.Rule.create('div.ola::after {color: blue; font-size: 2em;}');
 assert.strictEqual(r.clone().toString(), r.toString());
 
-r = css.Value.create('1px 2px 3px rgba(0, 0, 0, 0.3)');
+r = stylecow.Value.create('1px 2px 3px rgba(0, 0, 0, 0.3)');
 assert.strictEqual(r.toString(), '1px 2px 3px rgba(0, 0, 0, 0.3)');
 
-r = css.Declaration.create('box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.3)');
+r = stylecow.Declaration.create('box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.3)');
 assert.strictEqual(r.toString(), 'box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.3);');
 
-r = css.Function.create('color(blue alpha(0.3))');
+r = stylecow.Function.create('color(blue alpha(0.3))');
 assert.strictEqual(r.toString(), 'color(blue alpha(0.3))');
 
-r = css.AtRule.create('@media (min-width: 500px) {color: blue;}');
+r = stylecow.AtRule.create('@media (min-width: 500px) {color: blue;}');
 assert.strictEqual(r.name, 'media');
 assert.strictEqual(r.length, 2);
--- tests/utils.js
+++ tests/utils.js
@@ -1,28 +1,27 @@
 var assert = require('assert');
-var utils  = require('../lib/utils');
-
+var stylecow  = require('../lib');
 var r;
 
-r = utils.explode(' ', 'one (two three) "four"');
+r = stylecow.utils.explode(' ', 'one (two three) "four"');
 assert.deepEqual(r, ['one', '(two three)', '"four"']);
 
-r = utils.explode(' ', 'one (two three) "four"', 2);
+r = stylecow.utils.explode(' ', 'one (two three) "four"', 2);
 assert.deepEqual(r, ['one', '(two three) "four"']);
 
-r = utils.explode(' ', 'one (two \'three \"four"');
+r = stylecow.utils.explode(' ', 'one (two \'three \"four"');
 assert.deepEqual(r, ['one', '(two \'three \"four"']);
 
-r = utils.explodeTrim(';', 'one ; two; three;');
+r = stylecow.utils.explodeTrim(';', 'one ; two; three;');
 assert.deepEqual(r, ['one', 'two', 'three']);
 
-r = utils.explode('{', 'div.ola {font-size: 3em; a {color: blue;}}', 2);
+r = stylecow.utils.explode('{', 'div.ola {font-size: 3em; a {color: blue;}}', 2);
 assert.deepEqual(r, ['div.ola ', 'font-size: 3em; a {color: blue;}}']);
 
-r = utils.explodeTrim(',', 'one');
+r = stylecow.utils.explodeTrim(',', 'one');
 assert.deepEqual(r, ['one']);
 
-r = utils.parseSelector('div.ola:not(.active)>img');
+r = stylecow.utils.parseSelector('div.ola:not(.active)>img');
 assert.deepEqual(r, ['div', '.ola', ':not(.active)', '>', 'img']);
 
-r = utils.parseSelector(':not(.active)');
+r = stylecow.utils.parseSelector(':not(.active)');
 assert.deepEqual(r, [':not(.active)']);

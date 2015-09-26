becf8b93755d16e08dd7e3501a9af05b124ccdd4
oscarotero <oom@oscarotero.com>
2015-09-26 21:43:26 +0200

    use stylecow-plugins package instead all plugins individually

--- tests/cases.js
+++ tests/cases.js
@@ -1,41 +1,24 @@
-var assert = require('assert');
-var stylecow = require('stylecow-core');
-
-var test = new stylecow.Test(__dirname + '/cases');
-var tasks = (new stylecow.Tasks())
-	.use(require('stylecow-plugin-calc'))
-	.use(require('stylecow-plugin-color'))
-	.use(require('stylecow-plugin-custom-media'))
-	.use(require('stylecow-plugin-custom-selector'))
-	.use(require('stylecow-plugin-extend'))
-	.use(require('stylecow-plugin-fixes'))
-	.use(require('stylecow-plugin-flex'))
-	.use(require('stylecow-plugin-import'))
-	.use(require('stylecow-plugin-matches'))
-	.use(require('stylecow-plugin-msfilter-background-alpha'))
-	.use(require('stylecow-plugin-msfilter-linear-gradient'))
-	.use(require('stylecow-plugin-msfilter-transform'))
-	.use(require('stylecow-plugin-nested-rules'))
-	.use(require('stylecow-plugin-prefixes'))
-	.use(require('stylecow-plugin-rem'))
-	.use(require('stylecow-plugin-variables'))
-	.use(require('stylecow-plugin-webkit-gradient'))
-	.minSupport({
-		"explorer": 0,
-		"firefox": 0,
-		"chrome": 0,
-		"safari": 0,
-		"opera": 0,
-		"android": 0,
-		"ios": 0
-	});
-
+var assert   = require('assert'),
+    stylecow = require('stylecow-core'),
+    plugins  = require('stylecow-plugins'),
+    test     = new stylecow.Test(__dirname + '/cases'),
+    tasks    = (new stylecow.Tasks())
+                    .use(plugins())
+                    .minSupport({
+                        "explorer": 0,
+                        "firefox": 0,
+                        "chrome": 0,
+                        "safari": 0,
+                        "opera": 0,
+                        "android": 0,
+                        "ios": 0
+                    });
 
 test.run(function (test) {
-	tasks.run(test.css);
+    tasks.run(test.css);
 
-	describe('cases/' + test.name, function() {
-		it('should match output.css', function() {
+    describe('cases/' + test.name, function() {
+        it('should match output.css', function() {
             //test.writeString()
             test.assertString();
         });
@@ -44,5 +27,5 @@ test.run(function (test) {
             //test.writeAst()
             test.assertAst();
         });
-	});
+    });
 });
